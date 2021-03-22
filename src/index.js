df314d96821e23b4df887e1b0a37bcd52a39d9ea
Vse Putyom <bertraid777@gmail.com>
2021-03-22 21:47:57 +0300

    added new project, webpack, split into modules and configured

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable eol-last */
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-use-before-define */
+/* eslint-disable strict */
+
+import test from './modules/test';
+
+test();
+
---
+++ src/modules/test.js
@@ -0,0 +1,7 @@
+
+
+const test = () => {
+    alert("Тест WebPack");
+};
+
+export default test;
