38d799392e43733302955d2a82b4e61210f80fe5
Vse Putyom <bertraid777@gmail.com>
2021-03-23 17:07:36 +0300

    feat(togglePopup): add animate and display block for call

--- src/modules/togglePopup.js
+++ src/modules/togglePopup.js
@@ -1,6 +1,10 @@
+/* eslint-disable eol-last */
+/* eslint-disable no-use-before-define */
 const showPopup = () => {
     const freeVisitForm = document.getElementById('free_visit_form');
-    const formWrapper = freeVisitForm.querySelector('.form-wrapper');
+    const callbackForm = document.getElementById('callback_form');
+    const wrapperVisit = freeVisitForm.querySelector('.form-wrapper');
+    const wrapperCall = callbackForm.querySelector('.form-wrapper');
     const width = document.documentElement.clientWidth;
 
     let count = 50;
@@ -12,32 +16,50 @@ const showPopup = () => {
                 if (width > 768) {
                     freeVisitForm.style.display = 'block';
                     count = 50;
-                    popupRequst = requestAnimationFrame(showPopup);
+                    popupAnimate = requestAnimationFrame(showPopup);
                 } else if (width < 768) {
                     freeVisitForm.style.display = 'block';
-                    formWrapper.style.top = 11 + '%';
+                    wrapperVisit.style.top = 11 + '%';
                 }
             }
         } else {
             freeVisitForm.style.display = 'none';
         }
+        if (e.target.matches('.callback-btn')) {
+            const target = e.target;
+            if (target) {
+                if (width > 768) {
+                    callbackForm.style.display = 'block';
+                    count = 50;
+                    popupAnimate = requestAnimationFrame(showPopup);
+                } else if (width < 768) {
+                    callbackForm.style.display = 'block';
+                    wrapperCall.style.top = 11 + '%';
+                }
+            }
+        } else {
+            callbackForm.style.display = 'none';
+        }
+
         if (!e.target.matches('.form-content')) {
             const closeTarget = e.target.closest('.close_icon');
             if (closeTarget) {
                 freeVisitForm.style.display = 'none';
-                formWrapper.style.top = '';
+                callbackForm.style.display = 'none';
             }
         }
     });
 
-    let popupRequst;
+
+    let popupAnimate;
     const showPopup = () => {
-        popupRequst = requestAnimationFrame(showPopup);
+        popupAnimate = requestAnimationFrame(showPopup);
         count--;
         if (count > 10) {
-            formWrapper.style.top = count + '%';
+            wrapperVisit.style.top = count + '%';
+            wrapperCall.style.top = count + '%';
         } else {
-            cancelAnimationFrame(popupRequst);
+            cancelAnimationFrame(popupAnimate);
         }
     };
 
